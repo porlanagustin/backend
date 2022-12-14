d0e3fc6042ac465eb8268cae4d7e096af0da3ba9
Agustin <porlan.agustin@gmail.com>
2022-12-14 18:32:28 -0300

    websockets

--- Websockets/src/public/client.socket.js
+++ Websockets/src/public/client.socket.js
@@ -1,11 +1,19 @@
 const socket = io();
 
+
+// PRODUCTOS
 const poolProducts = document.getElementById("poolProducts");
 const productForm = document.getElementById("productForm");
 const nameProduct = document.getElementById("nameProduct");
 const priceProduct = document.getElementById("priceProduct");
 const imageProduct = document.getElementById("imageProduct");
+//CHAT
+const messagesPool = document.getElementById("messagesPool");
+const emailInput = document.getElementById("emailInput");
+const messageInput = document.getElementById("messageInput");
+const messageForm = document.getElementById("messageForm");
 
+// LOGICA PRODUCTOS
 productForm.addEventListener('submit', (event) => {
 
     event.preventDefault();
@@ -30,3 +38,27 @@ socket.on("server:products", products => {
             </tr>`;
     });
 });
+
+//LOGICA CHAT
+messageForm.addEventListener('submit', (event) => {
+    event.preventDefault();
+
+    const email = emailInput.value;
+    const message = messageInput.value;
+
+    socket.emit('client:message', {email, message});
+
+});
+
+socket.on("server:messages", messages =>{
+    messagesPool.innerHTML = "";
+
+    messages.forEach(message =>{
+        messagesPool.innerHTML += `
+        <tr>
+        <td>${message.email}</td>
+        <td>${message.message}</td>
+        </tr>`;
+    });
+});
+
--- Websockets/src/server.js
+++ Websockets/src/server.js
@@ -6,6 +6,7 @@ import { fileURLToPath } from 'url';
 const __dirname = dirname(fileURLToPath(import.meta.url));
 const app =  express();
 const products = [];
+const messages = [];
 
 app.use(express.static(__dirname + "/public"));
 
@@ -18,11 +19,18 @@ const io = new IOServer(expressServer);
 io.on('connection', (socket) => {
     console.log(`Nuevo cliente conectado ${socket.id}`);
 
-    socket.emit("server:products", products)
+    socket.emit("server:products", products);
+    socket.emit("server:messages", messages);
 
     socket.on("client:product", productData => {
         products.push(productData);
 
         io.emit('server:products', products);
     });
+
+    socket.on("client:message", messageData => {
+        messages.push(messageData);
+
+        io.emit('server:messages', messages);
+    })
 });
