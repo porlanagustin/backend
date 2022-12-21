2c9d7efe71654f250f8dac963f80691f179d2792
Agustin <porlan.agustin@gmail.com>
2022-12-21 20:25:49 -0300

    get listo

--- Primera Entrega/src/routes/products.route.js
+++ Primera Entrega/src/routes/products.route.js
@@ -1,9 +1,31 @@
-import { Router } from "express";
+import e, { Router } from "express";
+import upload from "../libs/multer.js";
 
 const router = Router();
 
+const products = [{
+    id: 1,
+    name: 'diamante'
+},
+{
+    id: 2,
+    name: 'oro'
+}];
+
+router.route('/:id').get((req,res) => {
+
+    const { id } = req.params;
+    const productSelect = products.find((product) => product.id === Number(id));
+
+    if(!productSelect){
+        return res.status(404).json({ status: "Not found"});
+    }
+
+    res.json(productSelect);
+});
+
 router.route('/').get((req,res) => {
-    res.send('get ok');
+    res.json(products);
 }).post((req,res) => {
     res.send('post ok');
 }).delete((req,res) => {
--- Primera Entrega/src/server.js
+++ Primera Entrega/src/server.js
@@ -2,23 +2,24 @@ import express, { json, urlencoded } from 'express';
 import { dirname } from 'path';
 import { fileURLToPath } from 'url';
 import productsRouter from "./routes/products.route.js";
-import cartRouter from "./routes/cart.route.js";
-import loginHtml from "./routes/login.route.js";
+import baseRouter from "./routes/base.route.js";
+import path from 'path';
+
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
 const app = express();
 
-app.use('/api/', loginHtml);
 app.use('/api/products', productsRouter);
-app.use('/api/cart', cartRouter);
+app.use('/', baseRouter);
 
 app.use(json());
 app.use(urlencoded({ extended: true }));
+app.use("/images", express.static(path.join(__dirname + "/uploads")));
 
-app.set("view engine", "ejs");
-app.set("views", __dirname + "/views");
-
-
-app.listen(8080, () => {
+app.listen(8080, (error) => {
+    if (error) {
+        console.log("Error al iniciar la app", error);
+    } else {
     console.log("Server 8080 activo");
+}
 });
