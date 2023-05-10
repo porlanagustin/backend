342cfdb0ff24447d725599cd5cd5f7a36389142c
porlanagustin <porlan.agustin@gmail.com>
2023-05-10 05:19:20 -0300

    agregando react render

--- Servidor en Deno/Deno/server2.ts
+++ Servidor en Deno/Deno/server2.ts
@@ -1,68 +1,50 @@
-import express, { Request, Response } from "npm:express";
-import {
-  bgBrightMagenta,
-  bgGreen,
-  bgRed,
-  bgYellow,
-  blue,
-  bold,
-  gray,
-  italic,
-  red,
-  white,
-} from "https://deno.land/std/fmt/colors.ts";
+import React, { useState } from 'https://dev.jspm.io/react/index.js';
+import ReactDOMServer from 'https://dev.jspm.io/react-dom/server.js';
+import { createApp } from 'https://deno.land/x/servest@v1.3.1/mod.ts';
 
-const app = express();
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
+const app = createApp();
+const validColors = ['red', 'blue', 'green', 'yellow', 'pink', 'purple'];
+let colors: string[] = [];
 
-const colors: string[] = [];
-const validColors = [
-  bgBrightMagenta,
-  bgGreen,
-  bgRed,
-  bgYellow,
-  blue,
-  bold,
-  gray,
-  italic,
-  red,
-  white,
-];
+app.handle('/', async (req) => {
+  if (req.method === 'POST') {
+    const params = await req.formData();
+    const color = params.get('color') || '';
+    if (validColors.includes(color)) {
+      colors.push(color);
+    }
+  }
 
-// Ruta para mostrar el formulario y la lista de colores
-app.get("/", (req: Request, res: Response) => {
-  const form = `
-    <form method="POST" action="/">
-      <label for="color">Ingrese un color:</label>
-      <input type="text" id="color" name="color">
-      <button type="submit">Agregar</button>
-    </form>
-  `;
-
-  const list = `
-    <ul>
-      ${colors
-        .map(
-          (color) =>
-            `<li style="background-color: black; color: ${validColors.find(
-              (validColor) => validColor.name === color
-            )}">${color}</li>`
-        )
-        .join("")}
-    </ul>
-  `;
-
-  res.send(`${form}${list}`);
+  await req.respond({
+    status: 200,
+    headers: new Headers({
+      'content-type': 'text/html; charset=UTF-8',
+    }),
+    body: ReactDOMServer.renderToString(
+      <html>
+        <head>
+          <meta charSet="utf-8" />
+          <title>Servest con React</title>
+        </head>
+        <body>
+          <form method="POST" action="/">
+            <label htmlFor="color">Ingrese un color:</label>
+            <input type="text" id="color" name="color" />
+            <button type="submit">Agregar</button>
+          </form>
+          {colors.length > 0 && (
+            <ul>
+              {colors.map((color, index) => (
+                <li key={index} style={{ color: color }}>
+                  {color}
+                </li>
+              ))}
+            </ul>
+          )}
+        </body>
+      </html>
+    ),
+  });
 });
 
-// Ruta para procesar el formulario
-app.post("/", (req: Request, res: Response) => {
-  const color = req.body.color;
-  colors.push(color);
-  res.send(`Color ${color} agregado.`);
-});
-
-app.listen(3000, () => {
-  console.log("Server listening port 3000");
-});
+app.listen({ port: 8080 });
