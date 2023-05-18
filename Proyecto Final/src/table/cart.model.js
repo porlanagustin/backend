d671e5b581059745cdd2466defab34b72552ff0d
porlanagustin <porlan.agustin@gmail.com>
2023-05-18 00:11:53 -0300

    proyectofinalaprobado

--- Proyecto Final/src/table/cart.model.js
+++
@@ -1,8 +0,0 @@
-import { model, Schema } from "mongoose";
-
-const cartSchema = Schema({
-  products: { type: Schema.Types.Array },
-  username: { type: String },
-});
-
-export const Carts = model("cart", cartSchema);
